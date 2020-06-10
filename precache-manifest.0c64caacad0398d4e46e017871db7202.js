fb0234bfcaccd652e478abae02edcaa52a9f5e1c
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-06-10 22:53:02 +0300

    Updates

---
+++ precache-manifest.0c64caacad0398d4e46e017871db7202.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff9ca6294e534eb5084dd015f259b89",
+    "url": "/markdown-preview/index.html"
+  },
+  {
+    "revision": "c4f5405d7b25d2846a36",
+    "url": "/markdown-preview/static/css/main.3d62dec9.chunk.css"
+  },
+  {
+    "revision": "f34a983bdd7c7dac1cca",
+    "url": "/markdown-preview/static/js/2.1aaebc98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-preview/static/js/2.1aaebc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f5405d7b25d2846a36",
+    "url": "/markdown-preview/static/js/main.0a0eb9d4.chunk.js"
+  },
+  {
+    "revision": "992c396bf69ae9e2482c",
+    "url": "/markdown-preview/static/js/runtime-main.a05c430e.js"
+  },
+  {
+    "revision": "4f6ab6c0777c4857e072d342b3551036",
+    "url": "/markdown-preview/static/media/arrows-alt-solid.4f6ab6c0.svg"
+  },
+  {
+    "revision": "5a696dd3a5d8bc89aef4b4172772b317",
+    "url": "/markdown-preview/static/media/compress-arrows-alt-solid.5a696dd3.svg"
+  },
+  {
+    "revision": "fce92e01408fe64ba6c802c13844bbfa",
+    "url": "/markdown-preview/static/media/edit.fce92e01.svg"
+  }
+]);
